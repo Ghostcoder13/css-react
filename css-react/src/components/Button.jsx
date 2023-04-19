e54ab2fd00a1d4b56ac4897d506ceac1b1b7cdf3
Ghostcoder13 <sou3sou@hotmail.fr>
2023-04-19 11:09:26 +0200

    card

---
+++ css-react/src/components/Button.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  color: black;
+  padding: 1rem 3rem;
+  border: none;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  cursor: pointer;
+  outline: none;
+  width: 100%;
+  margin: auto;
+  background-color: #165c3b;
+  background-image: linear-gradient(90deg, #165c3b, #293630);
+  margin-top: 1rem;
+`;
